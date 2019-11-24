c29d21b79725a3b76195fbbda1a0c6790700dbb3
exorcist <yag0da@yandex.ru>
2019-11-24 21:36:47 +0300

    fix

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,14 @@
 const Agent = require('socks-proxy-agent');
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN, {
-    telegram: {
-        agent: new Agent(process.env.PROXY_URL),
-    },
-});
-
+let bot;
+if (process.env.PROXY_URL) {
+    bot = new Telegraf(process.env.TELEGRAM_TOKEN, {
+        telegram: {
+            agent: new Agent(process.env.PROXY_URL),
+        },
+    });
+} else {
+    bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+}
 module.exports = { bot };
